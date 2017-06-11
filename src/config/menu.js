42febebab9c295a8600d8952625ee02469c501d0
yinxueyuan <838476901@qq.com>
2017-06-11 23:35:00 +0800

    fix: 导航数据

--- src/config/menu.js
+++ src/config/menu.js
@@ -63,6 +63,27 @@ const MENUDATA = {
           href: ''
         }
       ]
+    },
+    {
+      title: '前端监控室',
+      list: [
+        {
+          title: 'Issues',
+          href: ''
+        },
+        {
+          title: 'Overview',
+          href: ''
+        },
+        {
+          title: 'UserFeedback',
+          href: ''
+        },
+        {
+          title: 'Releases',
+          href: ''
+        }
+      ]
     }
   ]
 }
